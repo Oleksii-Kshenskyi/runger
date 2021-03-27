3d33016458d2e0587b83af566982e9c6debd3b72
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2021-03-27 16:51:28 +0200

    (#12) Switched from Amethyst to Bevy. Dark gray colored window works fine.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use bevy::{prelude::*, render::pass::ClearColor};
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
