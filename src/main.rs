e00ea99389799d8b1f567a78d4ddb45fa1ab5b22
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-03-01 22:30:39 +0200

    [#2] Basic black empty window in new Bevy 0.13.0.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,7 @@
-use bevy::prelude::TextAlignment;
 use bevy::prelude::*;
 
 fn main() {
-    App::build()
-        .add_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-        .add_startup_system(setup.system())
+    App::new()
         .add_plugins(DefaultPlugins)
         .run();
-}
-
-fn setup(
-    commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands
-        .spawn(CameraUiBundle::default())
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                position_type: PositionType::Absolute,
-                align_items: AlignItems::FlexEnd,
-                justify_content: JustifyContent::Center,
-                ..Default::default()
-            },
-            material: materials.add(Color::NONE.into()),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn(TextBundle {
-                text: Text {
-                    value: "Hello Bevy World!!!".to_string(),
-                    font: asset_server.load("fonts/NotoSans-Regular.ttf"),
-                    style: TextStyle {
-                        font_size: 48.0,
-                        color: Color::rgb(0.7, 0.0, 0.0),
-                        alignment: TextAlignment {
-                            horizontal: HorizontalAlign::Center,
-                            vertical: VerticalAlign::Center,
-                        },
-                    },
-                },
-                style: Style {
-                    align_self: AlignSelf::Center,
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-        });
 }
