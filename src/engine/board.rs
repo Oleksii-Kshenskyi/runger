ee261f192331fac3eead70b142bd8407d1cf8e89
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-03-06 20:09:57 +0200

    [#2] Base structure for the system and board plugins.

---
+++ src/engine/board.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+pub struct GameBoardPlugin;
+
+impl Plugin for GameBoardPlugin {
+    fn build(&self, _app: &mut App) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+mod engine;
+
 use bevy::prelude::*;
+
+use engine::board::GameBoardPlugin;
+use engine::rsystem::BaseSystemPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(BaseSystemPlugin)
+        .add_plugins(GameBoardPlugin)
         .run();
 }
